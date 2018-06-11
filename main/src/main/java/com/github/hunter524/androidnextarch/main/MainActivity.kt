eda2fca1959538b48c9246decc47f51ef7b32bc6
hunter <guyuesh2@gmail.com>
2018-06-11 16:59:12 +0800

    Initial commit

---
+++ main/src/main/java/com/github/hunter524/androidnextarch/main/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.github.hunter524.androidnextarch.main
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        say_tv.setText("Hello Word")
+    }
+}
